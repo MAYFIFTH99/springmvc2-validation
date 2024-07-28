3903b25bfab8b55f56d91f545dbef4cb70e18a54
harvey <alstjr971@naver.com>
2024-07-29 03:19:15 +0900

    bean validation

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -10,18 +10,18 @@ import org.hibernate.validator.constraints.ScriptAssert;
 @Data
 //@ScriptAssert(lang = "javascript", script = "_this.price*_this.quantity >= 10000", message = "총합이 10000원이 넘어야 합니다")
 public class Item {
-    @NotNull(groups = UpdateCheck.class)
+//    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class,UpdateCheck.class})
+//    @NotBlank(groups = {SaveCheck.class,UpdateCheck.class})
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
-    @Range(min = 1000, max = 1000000)
+//    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000)
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
-    @Max(value = 9999,groups = {SaveCheck.class})
+//    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
+//    @Max(value = 9999,groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
